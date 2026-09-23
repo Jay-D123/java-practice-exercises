c5b909b3e72ac4616b5251acee27a44c45980a8f
Jay-D123 <rci.bsis.dometitajethro@gmail.com>
2026-09-23 14:02:02 +0800

    WhileLoop Ascending and Descending Practice Activity

---
+++ WhileDescendingPrinter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WhileDescendingPrinter {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+
+    int count = number;
+    while (count>=1) {
+      System.out.println(count);
+      count--;
+    }
+    input.close();
+  }
+}
---
+++ WhileLoopCounter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WhileLoopCounter {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+
+    int count = 1;
+    while (count<=number) {
+      System.out.println(count);
+      count++;
+    }
+    input.close();
+  }
+}
